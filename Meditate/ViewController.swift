b0f5dd07897b2628efef6868b28cb1117c990c5f
Trent251 <trentbitterman@comcast.net>
2017-03-21 12:57:25 -0600

    Testing...

--- Meditate/ViewController.swift
+++ Meditate/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
 
@@ -19,7 +20,44 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    class Sound {
+        var soundEffect: SystemSoundID = 0
+        init(name: String, type: String) {
+            let path  = Bundle.main.path(forResource: name, ofType: type)!
+            let pathURL = NSURL(fileURLWithPath: path)
+            AudioServicesCreateSystemSoundID(pathURL as CFURL, &soundEffect)
+        }
+        func play() {
+            AudioServicesPlaySystemSound(soundEffect)
+        }
+    }
+    
+    //Creating the sounds that will play
+    let beginSound = Sound(name: "beginSound", type: "aif")
+    let endSound = Sound(name: "endSound", type: "aif")
+    
+    
+    @IBAction func oneMinuteButton(_ sender: UIButton) {
+        meditate(length: 60)
+    }
+    
+    @IBAction func fiveMinuteButton(_ sender: UIButton) {
+        meditate(length: 300)
+    }
+    
+    @IBAction func tenMinuteButton(_ sender: UIButton) {
+        meditate(length: 600)
+    }
+    
+    @IBAction func fifteenMinuteButton(_ sender: UIButton) {
+        meditate(length: 900)
+    }
+    
+    func meditate(length: UInt32) {
+        beginSound.play()
+        sleep(length)
+        endSound.play()
+    }
 }
 
